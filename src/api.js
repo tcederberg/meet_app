b82983d1f319d63d878ac92275f954f5fdbc6438
tcederberg <trcederberg@gmail.com>
2024-01-04 11:16:47 -0500

    store events in localStorage & return if offline

--- src/api.js
+++ src/api.js
@@ -60,6 +60,12 @@ export const getEvents = async () => {
     if (window.location.href.startsWith("http://localhost")) {
     return mockData;
     }
+
+    if (!navigator.onLine) {
+        const events = localStorage.getItem('lastEvents');
+        return events ? JSON.parse(events) : [];
+    }
+
     const token = await getAccessToken();
 
     if (token) {
@@ -68,6 +74,7 @@ export const getEvents = async () => {
         const response = await fetch(url);
         const result = await response.json();
         if (result) {
+            localStorage.setItem('lastEvents', JSON.stringify(result.events));
             return result.events;
         } else return null;
     }
